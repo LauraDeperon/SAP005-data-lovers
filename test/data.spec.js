911d06763d132e446e7aa1efc5954dd70f21e759
LuanaGss <luana_gss@hotmail.com>
2020-12-04 13:32:50 -0300

    Responsivo

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,56 @@
-import { example, anotherExample } from '../src/data.js';
+import { calculoAgregado, filtroTipo, ordenarPoke, pesquisarPoke } from '../src/data.js';
+import { alunas, Carol, Amanda, Natasha } from '../src/api.js';
 
-
-describe('example', () => {
+describe('function pesquisarPoke', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof pesquisarPoke).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns ´pesquisarPoke´', () => {
+    expect(pesquisarPoke(alunas, "nat")).toStrictEqual([Natasha]);
   });
 });
 
-
-describe('anotherExample', () => {
+describe('function ordenarPoke', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof ordenarPoke).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns ´ordenarPoke´ for case "ordemA-Z"', () => {
+    expect(ordenarPoke(alunas, "ordemA-Z")).toStrictEqual([Amanda, Carol, Natasha]);
+  });
+
+  it('returns ´ordenarPoke´ for case ordemZ-A', () => {
+    expect(ordenarPoke(alunas, "ordemZ-A")).toStrictEqual([Natasha, Carol, Amanda]);
+  });
+
+  it('returns ´ordenarPoke´ for case "ordemCrescente', () => {
+    expect(ordenarPoke(alunas, "ordemCrescente")).toStrictEqual([Carol, Amanda, Natasha]);
+  });
+});
+
+describe('function filtroTipo', () => {
+  it('is a function', () => {
+    expect(typeof filtroTipo).toBe('function');
+  });
+
+  it('returns ´filtroTipo´', () => {
+    expect(filtroTipo(alunas, "futebol")).toStrictEqual([Amanda, Natasha]);
+  });
+});
+
+describe(' function calculoAgregado', () => {
+  it('is a function', () => {
+    expect(typeof calculoAgregado).toBe('function');
+  });
+
+  it('returns ´calculoAgregado´', () => {
+    expect(calculoAgregado( 160, 175)).toBe(91);
+  });
+  it('should throw TypeError when invoked with wrong argument types', () => {
+    expect(() => calculoAgregado()).toThrow(TypeError);
+    expect(() => calculoAgregado(" ", " ")).toThrow(TypeError);
+    expect(() => calculoAgregado(null, [])).toThrow(TypeError);
+    expect(() => calculoAgregado(0, " ")).toThrow(TypeError);
   });
 });
